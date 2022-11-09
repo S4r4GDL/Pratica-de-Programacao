78adbafd8cf2110015bdab7958db33c269ebd7bd
S4r4GDL <111251947+S4r4GDL@users.noreply.github.com>
2022-11-09 11:27:30 -0300

    Update Q01.php

--- Prova/Q01/Q01.php
+++ Prova/Q01/Q01.php
@@ -1,11 +1,12 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <link link rel='stylesheet' href='Q01.css'>
 <link rel="icon" type="image/x-icon" href="Q01.ico">
 <body>
+<?php Repetir();?>
 <form method="get" action ="Q01.html">
     <label aling="center">
-        <?php Repetir();?>
         <input type="submit" value='Voltar'/>
     </label>
 </form>
@@ -13,15 +14,22 @@
 </html>
 <?php
 function Repetir(){
-    if(isset($_GET['times']) && $_GET['times'] != ''  && $_GET['times'] > 0)
+    if(isset($_GET['times']))
     {
-    	$times=$_GET['times'];
-        $str="<h2>Seja bem vindo</h2>";
-        echo str_repeat($str,$times);
+        $times=$_GET['times'];
+        if($times > 0)
+        {
+            $str="<h2>Seja bem vindo</h2>";
+            echo str_repeat($str, $times);
+        }
+        else
+        {
+            echo "<h2>Valor inválido</h2>";
+        }
     }
     else
     {
-     echo"<h2Nenhum valor inserido</h2>";
+        echo "<h2>Nenhum valor informado!</h2>";
     }
-}    
+}
 ?>
